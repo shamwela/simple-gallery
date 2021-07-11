d8c9d292acc1a42da8cd7009a384522aa1276738
Sha Mwe La <shamwela2002@gmail.com>
2021-07-11 14:04:20 +0630

    Implement basic features

--- src/App.js
+++ src/App.js
@@ -1,11 +1,33 @@
-import './App.sass';
+import React, { Component } from 'react'
+import './App.sass'
 
-function App() {
-  return (
-    <div>
-      
-    </div>
-  );
+class App extends Component {
+  state = {
+    src: undefined,
+  }
+
+  handleClick = ({ currentTarget }) => {
+    this.setState({ src: currentTarget.src })
+  }
+
+  render() {
+    const { src } = this.state
+
+    return (
+      <div id='container'>
+        <div id='app'>
+          <img src={src} id='selected' />
+          <div>
+            <img
+              onClick={this.handleClick}
+              src='https://source.unsplash.com/WLUHO9A_xik/1600x900'
+              style={{ cursor: 'pointer' }}
+            />
+          </div>
+        </div>
+      </div>
+    )
+  }
 }
 
-export default App;
+export default App
